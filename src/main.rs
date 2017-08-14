e6eb000c3b1f1e6149096ac34a85515e8419011a
Manuel Franco <mafraba@gmail.com>
2017-08-15 00:14:03 +0200

    Begin chip8 implementation.
    For the moment, model memory and registers, and provide a way to load data.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 extern crate clap;
 
 use clap::{Arg, App};
@@ -30,5 +33,50 @@ fn main() {
     let mut ch8_buffer = Vec::new();
     ch8_file.read_to_end(&mut ch8_buffer).expect("Failed to read ROM");
 
+    // Create runtime
+    let mut ch8core = Chip8Core::new();
+
+    // Load file to memory
+    ch8core.load(&ch8_buffer);
+
     println!("To be continued ... :)");
+}
+
+// Implementation of a CHIP-8 runtime
+struct Chip8Core {
+    ram: [u8; 0x1000],  // 4K of memory
+    reg: [u8; 16],      // 16 registers
+    i: u16,             // Memory address register
+    sp: u16,            // Stack pointer
+    t_delay: u8,        // Delay timer
+    t_sound: u8,        // Sound timer
+    pc: u16             // Program counter
+}
+
+// Default values for a Chip8Core fileds
+impl Default for Chip8Core {
+    fn default() -> Chip8Core {
+        Chip8Core {
+            ram: [0; 0x1000],
+            reg: [0; 16],
+            i: 0,
+            sp: 0xEA0,
+            t_delay: 0,
+            t_sound: 0,
+            pc: 0x200
+        }
+    }
+}
+
+// Methods for a Chip8Core
+impl Chip8Core {
+    // Create a new brand Chip8Core instance
+    fn new() -> Chip8Core {
+        Default::default()
+    }
+
+    // Load data onto RAM
+    fn load(&mut self, data: &[u8]) {
+        (&mut self.ram[0x200..(0x200+data.len())]).copy_from_slice(data);
+    }
 }
---
+++ src/tests.rs
@@ -0,0 +1,19 @@
+use super::*;
+
+#[test]
+fn load_data() {
+    let mut ch8core = Chip8Core::new();
+    assert_eq!(ch8core.ram[0x200], 0);
+    assert_eq!(ch8core.ram[0x201], 0);
+    assert_eq!(ch8core.ram[0x202], 0);
+    assert_eq!(ch8core.ram[0x203], 0);
+
+    let data: &[u8] = &[1,2,3,4];
+    ch8core.load(data);
+    assert_eq!(ch8core.ram[0x199], 0);
+    assert_eq!(ch8core.ram[0x200], 1);
+    assert_eq!(ch8core.ram[0x201], 2);
+    assert_eq!(ch8core.ram[0x202], 3);
+    assert_eq!(ch8core.ram[0x203], 4);
+    assert_eq!(ch8core.ram[0x204], 0);
+}
